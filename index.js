9f0a8a8d26ec81ec6b8d135824eaade8acfc148d
Matheus Costa Vieira <m.vieira@cinq.com.br>
2016-11-08 15:36:42 -0200

    update code to use arrow function and improve doc

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const resolve = require('path').resolve;
 const sync = require('glob').sync;
 
 /**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
+ * Require files sync and execute a callback after require
+ * @param {string} path the pattern to find the file(s)
+ * @param {function} callback an callbck to execute for each file
  */
-module.exports = function requireFiles(path, callback) {
+module.exports = (path, callback = x => x) =>
     sync(path)
       .map((p) => resolve(p))
       .map((p) => require(p))
-      .map(callback);
+      .map(callback)
 };
